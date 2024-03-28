1e8b0fa4b9d8cc98aa3d933a4bc8e7e4c33ab0cc
Damiano Fassina <d.fassina18@imperial.ac.uk>
2024-03-28 17:14:24 +0000

    Added load model and preprocess image functions

--- utils/helper.py
+++ utils/helper.py
@@ -6,3 +6,31 @@ import collections
 import torch.nn.functional as F
 from torchvision import transforms
 from PIL import Image
+
+# Constants
+MEAN = [0.485, 0.456, 0.406]
+STD = [0.229, 0.224, 0.225]
+EPSILON = 0.25
+NUM_STEPS = 5
+ALPHA = 0.025
+
+
+def load_model():
+    """Load and return the pre-trained ResNet50 model."""
+    model = torchvision.models.resnet50(pretrained=True)
+    model.eval()
+    return model
+
+def preprocess_image(image_path, mean=MEAN, std=STD):
+    """Preprocess the input image."""
+    preprocess = transforms.Compose([
+        transforms.Resize(256),
+        transforms.ToTensor(),
+        transforms.Normalize(mean, std),
+    ])
+    img = Image.open(image_path)
+    input_image = preprocess(img)
+    input_image = input_image.unsqueeze(0)
+    input_image.requires_grad = True
+    return input_image
+
